d644a774b32d5a91bf996b81f19cf112a0d033e3
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-06-21 16:18:19 +0000

    First draft

--- src/app.js
+++ src/app.js
@@ -1,11 +1,65 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+document.onreadystatechange = function(event) {
+  if (document.readyState !== "complete") return;
+  drawCard();
 };
+
+function drawCard() {
+  const randomSuit = chooseRandomItemFrom(suits());
+  const randomCardValue = chooseRandomItemFrom(cardValues());
+
+  const suitSpans = document.querySelectorAll(".suit");
+  for (const span of suitSpans) {
+    span.textContent = randomSuit.label;
+    span.style.color = randomSuit.color;
+  }
+
+  const cardValue = document.querySelectorAll(".cardValue");
+  for (const value of cardValue) {
+    value.textContent = randomCardValue;
+  }
+}
+
+function chooseRandomItemFrom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function cardValues() {
+  return [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+}
+
+function suits() {
+  return [
+    {
+      label: "♥",
+      color: "red"
+    },
+    {
+      label: "♣",
+      color: "white"
+    },
+    {
+      label: "♠",
+      color: "white"
+    },
+    {
+      label: "♦",
+      color: "red"
+    }
+  ];
+}
